7d449fb9d1bc563aca869f89debfced7f08298b4
Fatineachtouk <afatine919@gmail.com>
2026-09-11 16:34:34 +0100

    move dbt project into dbt/ folder

---
+++ target/compiled/dbt_elt_tpch/models/marts/int_order_items.sql
@@ -0,0 +1,18 @@
+select
+    li.order_item_key,
+    li.part_key,
+    li.line_number,
+    li.extended_price,
+    o.order_key,
+    o.customer_key,
+    o.order_date,
+    
+    (-1 * li.extended_price * li.discount_percentage)::decimal(16, 2)
+ AS item_discount_amount
+from
+    dbt_db.dbt_schema.stg__tpch_orders o
+join
+    dbt_db.dbt_schema.stg__tpch_line_items li
+        on o.order_key = li.order_key
+order by
+    o.order_date
